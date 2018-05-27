38d562d75f3725efc154ca731645c49d65a4a1a5
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-05-27 13:21:39 -0400

    Add slock

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -17,8 +17,8 @@ main = do
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((controlMask .|. mod1Mask, xK_Delete), spawn "xset dpms 0 0 2; xset dpms force off; slock && xset dpms 0 0 60")
         , ((0, xK_Print), spawn "scrot")
         , ((0, 0x1008FF11), spawn "amixer -c 1 -q sset Master 2%-")
         , ((0, 0x1008FF13), spawn "amixer -c 1 -q sset Master 2%+")
